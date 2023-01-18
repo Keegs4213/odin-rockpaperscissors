728252a3f8bd97f4d8da6bb893d08ed72a26d819
Keegs4213 <keegs4213@gmail.com>
2023-01-19 12:46:53 +1300

    tidied up first function, started round function

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-// Function to make the computer randomly choose Rock paper or scissors//
-
+// Function to make the computer randomly choose rock paper or scissors//
+const getRandomValue = Math.random();
 
 function getComputerChoice() {
-let rock = "Rock";
-let paper = "Paper";
-let scissors = "Scissors";
-let getRandomValue = Math.random();
+    let rock = "Rock";
+    let paper = "Paper";
+    let scissors = "Scissors";
+    let getRandomValue = Math.random();
+    //console.log(getRandomValue);
+    if (getRandomValue <= 0.33) {
+        return rock;
+    } else if (getRandomValue <= 0.66) {
+        return paper;
+    } else {
+        return scissors;
+    }
 }
-if (getRandomValue <= 0.33) {
-    return rock;
-} 
-else if (getRandomValue <= 0.66) {
-    return paper;
-} else {
-    return scissors
+
+//Function plays single round of RPS
+function round(playerSelection,computerSelection){
+
+
 }
-console.log(getComputerChoice);
+    
